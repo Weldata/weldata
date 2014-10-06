e59415263793c33cb70256013e8cc76101b8b274
Sharanc25 <Sharanc25@gmail.com>
2014-10-06 19:34:24 +0530

    Before MetaTags module Update

---
+++ sites/all/modules/weldata/scripts/weldata.js
@@ -0,0 +1,13 @@
+(function ($) {
+    Drupal.behaviors.weldata =  {
+        attach: function(context, settings) {
+//******************************Begin my code
+
+            //$abstractPage_title = $('body.section-submit-abstract h1#page-title',context).text("Submit Abstract");
+            //$paperPage_title = $('body.section-upload-full-paper h1#page-title',context).text("Submit Full Paper");
+
+
+//******************************I'm done with my code
+        }
+    };
+})(jQuery);
