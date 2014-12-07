735928ee3af13c1edc3506e134f8cea2c449d2bc
Sharanc25 <Sharanc25@gmail.com>
2014-12-07 20:13:38 +0530

    Adds Comments to template.php

--- sites/all/themes/weldata/template.php
+++ sites/all/themes/weldata/template.php
@@ -3,77 +3,42 @@
 /**
  * @file
  * Template.php - process theme data for your sub-theme.
- * 
- * Rename each function and instance of "footheme" to match
- * your subthemes name, e.g. if you name your theme "footheme" then the function
- * name will be "footheme_preprocess_hook". Tip - you can search/replace
- * on "footheme".
+ */
+
+/**
+ * Implements hook_preprocess_print
+ * Adds Variables for PQR Table
  */
 function weldata_preprocess_print(&$variables) {
-  $vars = $variables;
   $node = $variables['node'];
+  $display = array ('label' => 'hidden');
   if($node->type == 'pqr_wps'){
-    $variables['type'] =  field_view_field('node', $node, 'field_type_or_grade');
+    $variables['date'] =  field_view_field('node', $node, 'field_date',$display);
+    $variables['revision'] =  field_view_field('node', $node, 'field_revision',$display);
+    $variables['wps_used'] =  field_view_field('node', $node, 'field_wps_number',$display);
+    $variables['welding_process'] =  field_view_field('node', $node, 'field_welding_process',$display);
+    $variables['brazing_process'] =  field_view_field('node', $node, 'field_brazing_process',$display);
+    $variables['process_type'] =  field_view_field('node', $node, 'field_process_type',$display);
+
+    //Base Metals Table
+    $variables['material_specification'] =  field_view_field('node', $node, 'field_material_specification',$display);
+    $variables['type_grade_uns'] =  field_view_field('node', $node, 'field_type_or_grade',$display);
+    $variables['thickness_test_coupon'] =  field_view_field('node', $node, 'field_thickness_of_test_coupon',$display);
+    $variables['diameter_test_coupon'] =  field_view_field('node', $node, 'field_diameter_of_test_coupon',$display);
+    $variables['maximum_pass_thickness'] =  field_view_field('node', $node, 'field_maximum_pass_thickness',$display);
+    $variables['other_base_metals'] =  field_view_field('node', $node, 'field_other_base_metal',$display);
+
+    //PWHT Table
+    $variables['pwht_temperature'] =  field_view_field('node', $node, 'field_temperature',$display);
+    $variables['pwht_time'] =  field_view_field('node', $node, 'field_time',$display);
+    $variables['other_pwht'] =  field_view_field('node', $node, 'field_other_pwht',$display);
   }
 }
 
-
+/**
+ * Implements hook_date_combo
+ * Removes Fieldset from date fields
+ */
 function weldata_date_combo($variables) {
   return theme('form_element', $variables);
 }
-
-
-
-/**
- * Override or insert variables for the html template.
- */
-/* -- Delete this line if you want to use this function
-function footheme_preprocess_html(&$vars) {
-}
-function footheme_process_html(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables for the page templates.
- */
-/* -- Delete this line if you want to use these functions
-function footheme_preprocess_page(&$vars) {
-}
-function footheme_process_page(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the node templates.
- */
-/* -- Delete this line if you want to use these functions
-function footheme_preprocess_node(&$vars) {
-}
-function footheme_process_node(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the comment templates.
- */
-/* -- Delete this line if you want to use these functions
-function footheme_preprocess_comment(&$vars) {
-}
-function footheme_process_comment(&$vars) {
-}
-// */
-
-
-/**
- * Override or insert variables into the block templates.
- */
-/* -- Delete this line if you want to use these functions
-function footheme_preprocess_block(&$vars) {
-}
-function footheme_process_block(&$vars) {
-}
-// */
